88efb5007f8bfd43f8992453fa7ede0edbfbe3ec
webtechmoz <zoidycine@gmail.com>
2025-02-23 01:26:21 +0200

    pyweber 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyweber',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(include=['pyweber', 'pyweber.*']),
     install_requires=[
         'websockets',
