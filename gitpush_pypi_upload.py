2eebe39dbb25eb8c41f574801a155520ac5cb040
webtechmoz <zoidycine@gmail.com>
2025-02-23 16:43:59 +0200

    pyweber 0.1.7

---
+++ gitpush_pypi_upload.py
@@ -0,0 +1,15 @@
+from gitpush import subprocess, push
+import shutil
+
+def upload_module():
+    print('\n============ GIT PUSH ============')
+
+    shutil.rmtree(path='dist', ignore_errors=True)
+
+    subprocess.run(['python', '-m', 'build'], shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(['twine', 'upload', 'dist/*'], shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    push()
+
+if __name__ == '__main__':
+    upload_module()
